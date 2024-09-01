115d0295f11d6b422a96e3abe33b7a7fd113188e
HughKinnett <124326537+HughKinnett@users.noreply.github.com>
2024-08-31 22:19:40 -0400

    Added script tags to the login page

--- loginfirebase.js
+++ loginfirebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { GoogleAuthProvider } from "firebase/auth";
 
