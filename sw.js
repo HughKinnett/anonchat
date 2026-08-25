0a4fa50649b98b0e07c8e9336666b4363be1824f
HughKinnett <124326537+HughKinnett@users.noreply.github.com>
2026-08-24 21:19:00 -0400

    Cache legacy migration module

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "anonchat-v4";
+const CACHE_NAME = "anonchat-v5";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -7,6 +7,7 @@ const APP_SHELL = [
   "./login.css",
   "./timeline.css",
   "./firebase-config.js",
+  "./legacy-profile.js",
   "./loginfirebase.js",
   "./timeline.js",
   "./profile.js",
