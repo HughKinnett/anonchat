98114cd188fc08033fe778f81b67f9917026a762
HughKinnett <124326537+HughKinnett@users.noreply.github.com>
2026-09-05 17:43:03 -0400

    fix: cache mobile layout hotfix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "anonchat-v128";
+const CACHE_NAME = "anonchat-v129";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -30,6 +30,7 @@ const APP_SHELL = [
   "./login.css",
   "./controls.css",
   "./timeline.css",
+  "./mobile-hotfix.css",
   "./community.css",
   "./premium.css",
   "./payment-preparation.css",
