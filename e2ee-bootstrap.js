1d03cbdacfed0dee32c4a4f4cdf8d8808e06692a
HughKinnett <124326537+HughKinnett@users.noreply.github.com>
2026-09-06 12:33:22 -0400

    fix: align E2EE bootstrap with auth-loss cleanup

--- e2ee-bootstrap.js
+++ e2ee-bootstrap.js
@@ -1,9 +1,13 @@
 import { auth, db } from "./firebase-config.js";
 import { ensureE2eeIdentity } from "./e2ee-identity.js";
+import { exitAfterAuthLoss } from "./push-exit.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
-onAuthStateChanged(auth, (user) => {
-  if (!user) return;
+onAuthStateChanged(auth, async (user) => {
+  if (!user) {
+    await exitAfterAuthLoss({ redirect: () => {} });
+    return;
+  }
   void ensureE2eeIdentity(db, user).catch(() => {
     // Community surfaces report encryption readiness when the user tries to message.
   });
